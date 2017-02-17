61ce75eea4b04cbdac1fc1b4d6b82bad7cae8dff
vyshnevska <vyshnevska.n@gmail.com>
2017-02-17 13:56:13 +0100

    Change state of post component

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -1,8 +1,28 @@
 var Post = React.createClass({
+  getInitialState() {
+    return (
+      { clicked: false }
+    )
+  },
+
+  _showMe(){
+    var newState = {};
+    newState['clicked'] = true;
+    this.setState(newState);
+  },
 
   render: function() {
+    onOpen = <a onClick={this._showMe}>Show</a>;
+    if(this.state.clicked){
+      onOpen = <div>
+        <h2> {this.props.post.body} </h2>
+        <span> {this.props.post.published} </span>
+      </div>
+    }
     return (
-      <h2> Hello from react! </h2>
+      <div>
+        {onOpen}
+      </div>
     )
   }
 });
