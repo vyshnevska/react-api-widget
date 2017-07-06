a9d2edf36955d0a9463f103c4b5bffd0646de7e6
vyshnevska <vyshnevska.n@gmail.com>
2017-07-06 13:52:35 +0200

    Graphql User types

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,8 +4,8 @@ Types::UserType = GraphQL::ObjectType.define do
   description 'a user'
 
   field :id, !types.ID
-  field :username, !types.String, property: :username
-  field :fullName, !types.String, property: :full_name
+  field :username, types.String, property: :username
+  field :fullName, types.String, property: :full_name
   field :email, !types.String, 'Like a phone number, but spammier', property: :email
   field :friends, -> { types[Types::UserType] }, 'Some people to lean on'
 end
