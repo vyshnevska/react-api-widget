b59aaf6b4203c773bd5756a72b5381b44474862a
vyshnevska <vyshnevska.n@gmail.com>
2017-08-04 10:13:03 +0200

    Fix crash when post has no image. Fix creation of a channel

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,12 @@ module PostsHelper
   def author_select_options
     [[current_user.name, current_user.id]] + User.all.map{|u| [u.name, u.id] if u.id != current_user.id }
   end
+
+  def image_urls(post)
+    %w(top_image image_1 image_2).inject([]) do |arr, image_type|
+      arr << post.send(image_type).url(:medium)
+      arr.compact
+    end
+  end
+
 end
