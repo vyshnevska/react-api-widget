7e7c67378a2397c3e4c4cabcc3ef7e24dcc8ef10
vyshnevska <vyshnevska.n@gmail.com>
2017-08-25 20:28:29 +0200

    Redirect with msg if user sign ins with invalid credentials

--- app/controllers/custom_sessions_controller.rb
+++ app/controllers/custom_sessions_controller.rb
@@ -3,6 +3,11 @@ class CustomSessionsController < Devise::SessionsController
 
   def before_login
     user = User.find_by_email(params[:user][:email])
+
+    unless user
+      flash[:error] = 'Invalid email or password'
+      return redirect_to root_path
+    end
     user.generate_and_save_auth_token!
   end
 end
